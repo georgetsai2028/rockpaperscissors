debf2328d139db23ce069dab357ca6e8a8e04eee
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-10-31 14:16:00 -0700

    user input function made and starting game function

--- script.js
+++ script.js
@@ -7,5 +7,15 @@ function get_computer_choice()
 function get_user_choice()
 {
     let user_input = prompt("Enter: 0 for Rock, 1 for Paper, or 2 for Scissors")
+    return user_input;
 }
-console.log(get_computer_choice())
+
+function play_round(user_choice, computer_choice)
+{
+    
+}
+var computer_score = 0;
+var user_score = 0;
+let user_input = get_user_choice()
+
+console.log(user_input);
