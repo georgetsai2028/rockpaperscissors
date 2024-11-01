db211c0da13f866d5fcc207b3355be5ec209d232
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-10-31 20:53:04 -0700

    made play round function more readable and ensure that the point system will work

--- script.js
+++ script.js
@@ -24,54 +24,28 @@ function get_user_choice()
 
 function play_round(user_choice, computer_choice)
 {
-    var player_score = 0;
-    var computer_score = 0;
     //0 = Rock, 1 = Paper, 2 = Scissors
-    if (user_choice == 0 && computer_choice == 0)
+    if (user_choice ==  computer_choice)
     {
-        console.log("Tied");
+        console.log("Tied!");
+        return 0;
     }
-    else if (user_choice == 0 && computer_choice == 1)
+    else if ((user_choice == 0 && computer_choice == 2) || (user_choice == 1 && computer_choice == 0) || (user_choice == 2 && computer_choice == 1))
     {
-        console.log("Computer Wins");
-        computer_score += 1;
+        console.log("You win this round!!");
+        return 1;
     }
-    else if (user_choice == 0 && computer_choice == 2)
+    else
     {
-        console.log("User Wins");
-        user_score += 1;
-    }
-    else if (user_choice == 1 && computer_choice == 0)
-    {
-        console.log("User Wins");
-        user_score += 1;
-    }
-    else if (user_choice == 1 && computer_choice == 1)
-    {
-        console.log("Tied");
-    }
-    else if (user_choice == 1 && computer_choice == 2)
-    {
-        console.log("Computer Wins");
-        computer_score += 1;
-    }
-    else if (user_choice == 2 && computer_choice == 0)
-    {
-        console.log("Computer Wins");
-        computer_score += 1;
-    }
-    else if (user_choice == 2 && computer_choice == 1)
-    {
-        console.log("User Wins");
-        user_score += 1;
-    }
-    else if (user_choice == 2 && computer_choice == 2)
-    {
-        console.log("Tied");
+        console.log("Computer wins this round :(");
+        return -1;
     }
 }
-const user_input = get_user_choice()
-const computer_input = get_computer_choice()
 
-console.log(user_input);
-console.log(play_round(user_input, computer_input))
+let user_score = 0;
+let computer_score = 0;
+
+while (user_score <= 5 || computer_score <= 5)
+{
+    
+}
