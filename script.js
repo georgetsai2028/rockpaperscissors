690a6ab3f20070cca248141bb71639d40ce5b008
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-10-31 19:17:56 -0700

    play round function created

--- script.js
+++ script.js
@@ -24,10 +24,52 @@ function get_user_choice()
 
 function play_round(user_choice, computer_choice)
 {
-    
+    var player_score = 0;
+    var computer_score = 0;
+    //0 = Rock, 1 = Paper, 2 = Scissors
+    if (user_choice == 0 && computer_choice == 0)
+    {
+        console.log("Tied");
+    }
+    else if (user_choice == 0 && computer_choice == 1)
+    {
+        console.log("Computer Wins");
+        computer_score += 1;
+    }
+    else if (user_choice == 0 && computer_choice == 2)
+    {
+        console.log("User Wins");
+        user_score += 1;
+    }
+    else if (user_choice == 1 && computer_choice == 0)
+    {
+        console.log("User Wins");
+        user_score += 1;
+    }
+    else if (user_choice == 1 && computer_choice == 1)
+    {
+        console.log("Tied");
+    }
+    else if (user_choice == 1 && computer_choice == 2)
+    {
+        console.log("Computer Wins");
+        computer_score += 1;
+    }
+    else if (user_choice == 2 && computer_choice == 0)
+    {
+        console.log("Computer Wins");
+        computer_score += 1;
+    }
+    else if (user_choice == 2 && computer_choice == 1)
+    {
+        console.log("User Wins");
+        user_score += 1;
+    }
+    else if (user_choice == 2 && computer_choice == 2)
+    {
+        console.log("Tied");
+    }
 }
-var computer_score = 0;
-var user_score = 0;
 const user_input = get_user_choice()
 const computer_input = get_computer_choice()
 
