ddfa93cc0bdf7b0a5f5e3797d5f8ef2ade4b779b
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-10-31 18:49:18 -0700

    player input validation made

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ function get_computer_choice()
 function get_user_choice()
 {
     let user_input = prompt("Enter: 0 for Rock, 1 for Paper, or 2 for Scissors")
+    let count = 0;
+    while (count < 1)
+    {
+        if (user_input < 0 || user_input > 2)
+        {
+            user_input = prompt("Please only enter 0, 1, or 2")
+        }
+        else if (user_input >= 0 || user_input <= 2)
+        {
+            count++;
+        }
+    }
     return user_input;
 }
 
@@ -16,6 +28,8 @@ function play_round(user_choice, computer_choice)
 }
 var computer_score = 0;
 var user_score = 0;
-let user_input = get_user_choice()
+const user_input = get_user_choice()
+const computer_input = get_computer_choice()
 
 console.log(user_input);
+console.log(play_round(user_input, computer_input))
