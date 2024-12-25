1c73e3e0fcd251b61a75ff5846d8c10355e98f1e
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-12-24 18:34:00 -0800

    finished rps logic in that works in console. need to implement ui

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ buttonDiv.style.display = "flex";
 
 const rockButton = document.createElement("button");
 rockButton.textContent = "Rock";
+
 const paperButton = document.createElement("button");
 paperButton.textContent = "Paper";
+
 const scissorsButton = document.createElement("button");
 scissorsButton.textContent = "Scissors";
 
@@ -15,28 +17,14 @@ buttonDiv.appendChild(scissorsButton);
 
 document.body.appendChild(buttonDiv);
 
+let user_input;
+let user_score = 0;
+let computer_score = 0;
+
 function get_computer_choice()
 {
     let random = Math.floor(Math.random() * 3);
     return random;
-}
-
-function get_user_choice()
-{
-    buttonDiv.querySelectorAll("button");
-    buttonDiv[0].id = "rockButton";
-    buttonDiv[1].id = "paperButton";
-    buttonDiv[2].id = "scissorButton";
-
-    document.querySelector("#rockButton");
-    document.querySelector("#paperButton");
-    document.querySelector("#scissorsButton");
-
-    rockButton.addEventListener("click", user_input = 0);
-    paperButton.addEventListener("click", user_input = 1);
-    scissorsButton.addEventListener("click", user_input = 2);
-
-    return user_input;
 }
 function play_round(user_choice, computer_choice)
 {
@@ -44,57 +32,45 @@ function play_round(user_choice, computer_choice)
     if (user_choice ==  computer_choice)
     {
         console.log("Tied!");
-        return 0;
     }
     else if ((user_choice == 0 && computer_choice == 2) || (user_choice == 1 && computer_choice == 0) || (user_choice == 2 && computer_choice == 1))
     {
         console.log("You win this round!!");
-        return 1;
+        user_score++;
     }
     else
     {
         console.log("Computer wins this round :(");
-        return -1;
+        computer_score++;
     }
-}
-let choice = get_user_choice();
-console.log(user_choice);
-/*
-function play_game(){
-
-let user_score = 0;
-let computer_score = 0;
-
-while (user_score < 5 && computer_score < 5)
-{
-    let user_choice = get_user_choice();
-    let computer_choice = get_computer_choice();
-    let round_result = play_round(user_choice, computer_choice);
-    if (round_result == 1)
-    {
-        user_score += 1;
-    }
-    else if(round_result == -1)
-    {
-        computer_score += 1;
-    }
-    else if (round_result == 0)
-    {
-        user_score += 0;
-    }
-
     console.log(`User:  ${user_score} Computer:  ${computer_score}`);
-    
+
+    if (user_score == 5)
+        {
+            console.log("YOU WINNNNN!!!! :()");
+            return 0;
+        }
+        else if (computer_score == 5)
+        {
+            console.log("WOMP WOMP YOU STINKKKK, MY GRANDMA COULD DO BETTER")
+            return 0;
+        }
 }
 
-if (user_score == 5)
-{
-    console.log("YOU WINNNNN!!!! :()");
-}
-else if (computer_score == 5)
-{
-    console.log("WOMP WOMP YOU STINKKKK, MY GRANDMA COULD DO BETTER")
-}
-}
-console.log(play_game())
-*/
+
+
+rockButton.addEventListener("click", () => {
+    user_input = 0;
+    const computer_choice = get_computer_choice();
+    play_round(user_input, computer_choice);
+})
+paperButton.addEventListener("click", () => {
+    user_input = 1;
+    const computer_choice = get_computer_choice();
+    play_round(user_input, computer_choice);
+})  
+scissorsButton.addEventListener("click", () => {
+    user_input = 2;
+    const computer_choice = get_computer_choice();
+    play_round(user_input, computer_choice);
+})
